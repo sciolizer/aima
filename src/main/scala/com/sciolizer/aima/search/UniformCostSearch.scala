2f094a4453ce4a33c936c89ef10838561c3187a0
Joshua Ball <joshball@yahoo-inc.com>
2013-10-05 11:53:15 -0700

    Fixed UniformCostSearch (priority queue ordering was backward)

--- src/main/scala/com/sciolizer/aima/search/UniformCostSearch.scala
+++ src/main/scala/com/sciolizer/aima/search/UniformCostSearch.scala
@@ -5,8 +5,9 @@ import scala.collection.mutable
 class UniformCostSearch extends Search {
   def search[State, Action](problem: Problem[State, Action]): SearchResult[State, Action] = {
     var node: Node[State, Action] = ParentNode(problem.initialState)
+    val ordering: Ordering[Node[State, Action]] = Ordering.by(_.pathCost)
     //    if (problem.goalTest(node.state)) return Solution(node)
-    var frontier: mutable.PriorityQueue[Node[State, Action]] = mutable.PriorityQueue(node)(Ordering.by(_.pathCost))
+    var frontier: mutable.PriorityQueue[Node[State, Action]] = mutable.PriorityQueue(node)(ordering.reverse)
     var explored: Set[State] = Set()
     while (!frontier.isEmpty) {
       node = frontier.dequeue()
@@ -36,6 +37,14 @@ class UniformCostSearch extends Search {
 
 
 object TestPriorityQueuePicksLowestPathCost {
+  def main(args: Array[String]) {
+    val ordering: Ordering[ChildNode[Any, String]] = Ordering.by(_.pathCost)
+    val queue = mutable.PriorityQueue(
+      ChildNode("Hello", ParentNode(), "Goodbye", 1.0),
+      ChildNode("Hello", ParentNode(), "Goodbye", 2.0))(ordering.reverse)
+    println(queue.dequeue())
+  }
+
   //  def main(args: Array[String]) {
   //    val queue: mutable.PriorityQueue[ChildNode[String, String]] = mutable.PriorityQueue[ChildNode[String,String]]()(Ordering.ordered[ChildNode[String,String]])
   //    queue += ChildNode(null, null, null, 1.0)
